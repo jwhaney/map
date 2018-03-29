009108d21f3dd6e8f55f2a62e2b7936a9528d821
jwhaney <geojwh@gmail.com>
2018-03-29 09:53:40 -0500

    initial content upload; popup text modified

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # map
-leaflet map used for personal site
+
+a simple leaflet map of the austin area to embed in my personal website.
+
+- zoom to original extent button
+- zoom in and out button
+- openstreetmap base map
+- pin marking central austin with popup open by default
+
+![map image](images/map.png)
---
+++ js/map.js
@@ -0,0 +1,25 @@
+//create var bounds
+var northWest = L.latLng(30.404, -97.815),
+    southEast = L.latLng(30.189, -97.660),
+    bounds = L.latLngBounds(northWest, southEast);
+
+//create map - zoom to var bounds
+var map = L.map('map', { dragging: !L.Browser.mobile });
+map.fitBounds(bounds);
+
+//load osm basemap tiles
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//custom easy button control, zoom to var bounds
+L.easyButton('fa-globe fa-lg', function(){
+    map.fitBounds(bounds)
+}).addTo(map);
+
+//load centexgis marker location and popup info
+var marker = L.marker([30.27384, -97.74058]);
+
+var markerContent = "<i>I live here in</i><br>" + "<strong>Austin, TX</strong>";
+
+marker.addTo(map).bindPopup(markerContent, {autoClose:false}).openPopup();
