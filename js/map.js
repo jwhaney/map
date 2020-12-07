b4d3bcc1f674615ace8d328e344c97b65f0efa30
John Haney <geojwh@gmail.com>
2020-12-07 11:17:42 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -20,6 +20,6 @@ L.easyButton('fa-globe fa-lg', function(){
 //load centexgis marker location and popup info
 var marker = L.marker([30.27384, -97.74058]);
 
-var markerContent = "<i>I live here in</i><br>" + "<strong>Austin, TX</strong>";
+var markerContent = "<i>I live here in</i><br>" + "<strong>Austin, TX</strong><br><i>Hit me up on Github @jwhaney</i>";
 
 marker.addTo(map).bindPopup(markerContent, {autoClose:false}).openPopup();
